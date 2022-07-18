0bedec3b1889968921c6512867e3204d1b57d8b2
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-07-18 23:40:44 +0200

    Add minifier

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -1,7 +1,8 @@
-const fs = require("fs");
-const path = require("path");
-// const { minify } = require("minify");
-// console.log(minify);
+import fs from "fs";
+import fsPromise from "fs/promises";
+import path from "path";
+import { minify } from "minify";
+
 if (fs.existsSync("./dist")) {
     console.log("Build docs: Clearing dist folder");
     fs.rmSync("dist", {
@@ -20,20 +21,43 @@ if (!fs.existsSync("./dist/assets")) {
     fs.mkdirSync("./dist/assets");
 }
 
-const forFilesWithExtention = (dir, ext, onFound) => {
-    const content = fs.readdirSync(dir, { withFileTypes: true });
-    content.forEach(dirent => {
-        const filePath = dir + dirent.names;
-        if (dirent.isDirectory()) {
-            forFilesWithExtention(filePath, ext, onFound);
-        } else {
-            const fileExt = path.extname(filePath);
-            console.log(ext);
-            if (fileExt !== ext) {
-                onFound(filePath);
+const forFilesWithExtention = async (dir, ext, onFound) => {
+    let fileParsingTasks = [];
+    let folderTasks = [];
+    fs.readdirSync(dir, { withFileTypes: true })
+        .forEach(async (dirent) => {
+            const filePath = `${dir}/${dirent.name}`;
+            if (dirent.isDirectory()) {
+                folderTasks.push(
+                    forFilesWithExtention(filePath, ext, onFound)
+                );
+            } else if (path.extname(filePath) === ext) {
+                fileParsingTasks.push(onFound(filePath));
             }
-        }
+        });
+
+    const childrenFileParsingTasks = await Promise.all(folderTasks);
+    childrenFileParsingTasks.forEach((childFileParsingTasks) => {
+        fileParsingTasks = fileParsingTasks.concat(childFileParsingTasks);
     });
+
+    return fileParsingTasks;
 };
 
-// forFilesWithExtention("./dist", "html", (path) => minify(path));
+console.log("Build docs: minifying html files");
+forFilesWithExtention("./dist", ".html", async (path) => {
+    console.log("\t- " + path);
+    const output = await minify(path);
+    const file = await fsPromise.open(path, "w");
+    await file.writeFile(output);
+}).then(async (minifyPromises) => {
+    Promise.all(minifyPromises)
+        .then(() => {
+            console.log("Build docs: All HTML files minified succefully");
+        })
+        .catch((e) => {
+            console.log("Build docs: Something went wrong while minifying HTML files");
+            throw e;
+        });
+});
+
